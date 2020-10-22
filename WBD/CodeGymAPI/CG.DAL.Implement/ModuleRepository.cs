f950bc2a8a46d4361330148ec5b65e885044ab62
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-10-22 20:37:07 +0700

    HongSon-->Create IModuleRepository and ModuleRepository

---
+++ WBD/CodeGymAPI/CG.DAL.Implement/ModuleRepository.cs
@@ -0,0 +1,17 @@
+﻿using CG.DAL.Interface;
+using CG.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG.DAL.Implement
+{
+    class ModuleRepository : IModuleRepository
+    {
+        public Task<IEnumerable<ModuleViewModel>> Gets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
