31efc6b4e1477fc4fcb0e409da4f5dd01d67be73
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-10-22 22:16:35 +0700

    HongSon-->Create module.html and module.js

---
+++ CSS/script/module.js
@@ -0,0 +1,37 @@
+var module = module || {};
+
+module.apiUrl = "https://localhost:44393/api/module";
+
+module.drawTable = function() {
+    $.ajax({
+        url: `${module.apiUrl}/gets`,
+        method: 'GET',
+        dataType: 'JSON',
+        success: function(data) {
+            $.each(data, function(i, v) {
+                $('#tbModule>tbody').append(`
+                    <tr>
+                        <td>${v.moduleId}</td>
+                        <td>${v.moduleName}</td>
+                        <td>${v.duration}</td>
+                        <td>${v.status}</td>
+                        <td>${v.createdDate}</td>
+                        <td>${v.createdBy}</td>
+                        <td>${v.modifiedDate}</td>
+                        <td>${v.modifiedBy}</td>
+                        <td></td>
+                    </tr>
+                `);
+            })
+        }
+    });
+}
+
+module.init = function() {
+    module.drawTable();
+}
+
+
+$(document).ready(function() {
+    module.init();
+});
