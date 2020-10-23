4919561a053f94e63615ee4a53fc3095b5a65e55
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-10-23 10:32:16 +0700

    HongSon-->Alter function getmodulebyid

--- WBD/CodeGymAPI/CG.DAL.Implement/ModuleRepository.cs
+++ WBD/CodeGymAPI/CG.DAL.Implement/ModuleRepository.cs
@@ -9,9 +9,15 @@ namespace CG.DAL.Implement
 {
     public class ModuleRepository : BaseRepository, IModuleRepository
     {
-        public async Task<ModuleViewModel> GetModuleViewModelById(int id) =>
-            await SqlMapper.QueryFirstAsync<ModuleViewModel>(cnn: connection, sql: "sp_GetModuleById",
+        public async Task<ModuleViewModel> GetModuleViewModelById(int id)
+        {
+            DynamicParameters parameter = new DynamicParameters();
+            parameter.Add("@ModuleIdParm", id);
+
+            return await SqlMapper.QueryFirstAsync<ModuleViewModel>(cnn: connection, sql: "sp_GetModuleById", parameter,
                                                             commandType: CommandType.StoredProcedure);
+        }
+           
 
         public async Task<IEnumerable<ModuleViewModel>> Gets() =>
              await SqlMapper.QueryAsync<ModuleViewModel>(cnn: connection, sql: "sp_GetModules", commandType: CommandType.StoredProcedure);
