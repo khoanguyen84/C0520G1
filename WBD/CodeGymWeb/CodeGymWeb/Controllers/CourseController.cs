02d46cad3fa19ab16fd9464539f7d75ee20fe0d7
nhanvu9053 <nhanvu9053@gmail.com>
2020-10-30 16:06:55 +0700

    Nhan_UpdateCorse

--- WBD/CodeGymWeb/CodeGymWeb/Controllers/CourseController.cs
+++ WBD/CodeGymWeb/CodeGymWeb/Controllers/CourseController.cs
@@ -13,12 +13,11 @@ namespace CodeGymWeb.Controllers
             return View(data);
         }
 
-<<<<<<< HEAD
         public IActionResult Details(int id)
         {
             var data = ApiHelper<CourseView>.HttpGetAsync(@$"course/get/{id}");
             return View(data);
-=======
+        }
         [HttpGet]
         public IActionResult Create()
         {
@@ -34,12 +33,39 @@ namespace CodeGymWeb.Controllers
                 result = ApiHelper<SaveCourseRes>.HttpPostAsync("course/save", "POST", req);
                 if (result.CourseId != 0)
                 {
+                    TempData["Message"] = result.Message;
                     return RedirectToAction("index");
                 }
                 ModelState.AddModelError("", result.Message);
             }
             return View(req);
->>>>>>> origin/development
+
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var data = ApiHelper<SaveCourseReq>.HttpGetAsync(@$"course/get/{id}");
+            return View(data);
+        }
+        [HttpPost]
+        public  IActionResult Edit(SaveCourseReq req)
+        {
+            var result = new SaveCourseRes();
+            if (ModelState.IsValid)
+            {
+                result = ApiHelper<SaveCourseRes>.HttpPostAsync("course/save", "PATCH", req);
+                if (result.CourseId != 0)
+                {
+                    TempData["Message"] = "Course Edited";
+                    return Redirect($@"~/Course/Details/{result.CourseId}");
+                }
+                ModelState.AddModelError("", result.Message);
+
+            }
+            return View(req);
+
+        }
+
     }
 }
