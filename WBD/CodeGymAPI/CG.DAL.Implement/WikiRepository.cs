68462b523c8e20af206a6c3e7f812c327d4a3b73
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2020-10-30 16:00:50 +0700

    Create wiki table and wiki api

---
+++ WBD/CodeGymAPI/CG.DAL.Implement/WikiRepository.cs
@@ -0,0 +1,25 @@
+﻿using CG.DAL.Interface;
+using CG.Domain.Request.Course;
+using CG.Domain.Response.Course;
+using CG.Domain.Response.Wiki;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CG.DAL.Implement
+{
+    public class WikiRepository : BaseRepository, IWikiRepository
+    {
+        public async Task<IEnumerable<Status>> GetStatus(int tableId)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@TableId", tableId);
+            return await SqlMapper.QueryAsync<Status>(cnn: connection,
+                                                        sql: "sp_GetStatus",
+                                                        param: parameters,
+                                                        commandType: CommandType.StoredProcedure);
+        }
+    }
+}
