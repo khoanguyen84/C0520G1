48816760a2e6603a5badcc5a5ea4b0a100b103bf
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2020-11-02 17:26:10 +0700

    Create module API, try using jquey and ajax

---
+++ WBD/CodeGymAPI/CG.DAL.Interface/IModuleRepository.cs
@@ -0,0 +1,14 @@
+﻿using CG.Domain.Request.Course;
+using CG.Domain.Response.Course;
+using CG.Domain.Response.Module;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CG.DAL.Interface
+{
+    public interface IModuleRepository
+    {
+        Task<IEnumerable<ModuleView>> Gets();
+    }
+}
---
+++ WBD/CodeGymWeb/CodeGymWeb/Controllers/ModuleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeGymWeb.Models.Module;
+using CodeGymWeb.Ultilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeGymWeb.Controllers
+{
+    public class ModuleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/module/gets")]
+        public JsonResult Gets()
+        {
+            var modules = ApiHelper<List<ModuleView>>.HttpGetAsync("module/gets");
+            return Json(new { data = modules });
+        }
+    }
+}
